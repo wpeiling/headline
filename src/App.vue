4cd739305c7682ea82abcefe30c74ba2dd666878
wpeiling <501492630@qq.com>
2022-10-05 19:16:29 +0800

    初始化并梳理项目的结构

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <h1>头条</h1>
+    <div>
+      <i class="icon "></i>
+      <van-button type="primary">主要按钮</van-button>
+    </div>
   </div>
 </template>
 <script>
