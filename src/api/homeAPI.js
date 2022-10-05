0836fd4378c636ab2446590434e2d4211cac4f6f
wpeiling <501492630@qq.com>
2022-10-05 21:43:42 +0800

    实现了主页的布局

---
+++ src/api/homeAPI.js
@@ -0,0 +1,6 @@
+import request from '@/utils/request'
+
+// 请求用户频道列表数据的 API
+export const getUserChannelAPI = () => {
+  return request.get('/v1_0/user/channels')
+}
---
+++ src/views/Main/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-view></router-view>
+
+    <van-tabbar v-model="active" route>
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
